2757c32146ba4ac010cbf3c7bbfddd9d518f666c
WitchHat <jack111one1@gmail.com>
2026-09-16 09:35:54 -0400

    EXE Launcher

--- LaunchGame.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-chcp 65001 >nul
-pushd "%~dp0Resources" || exit /b 1
-"%~dp0Resources\modengine2_launcher.exe" -t er -c "%~dp0Resources\config_eldenring.toml"
-set "launchResult=%errorlevel%"
-popd
-if not "%launchResult%"=="0" pause
-exit /b %launchResult%
